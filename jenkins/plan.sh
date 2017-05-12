f26348b981289c302b9de7245a2ae05a3902e144
Paul Adams <paul@baggerspion.net>
2017-05-12 17:00:36 +0200

    Fix: run as root

--- jenkins/plan.sh
+++ jenkins/plan.sh
@@ -7,6 +7,7 @@ pkg_upstream_url=https://jenkins.io/
 pkg_source=http://mirrors.jenkins.io/war-stable/${pkg_version}/${pkg_name}.war
 pkg_shasum="aa7f243a4c84d3d6cfb99a218950b8f7b926af7aa2570b0e1707279d464472c7"
 pkg_deps=(core/jdk8)
+pkg_svc_user="root"
 
 do_unpack() {
     return 0
